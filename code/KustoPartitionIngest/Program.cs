f61aac2719a416e76f9ebfab4ca9b671c097c7c3
vplauzon <vincent_lauzon@yahoo.com.au>
2024-03-15 12:45:44 -0400

    Generalize to include pre-sharding

--- code/KustoPartitionIngest/Program.cs
+++ code/KustoPartitionIngest/Program.cs
@@ -4,30 +4,40 @@
     {
         static async Task Main(string[] args)
         {
-            if (args.Length < 5)
+            if (args.Length > 0)
             {
-                Console.Error.WriteLine("Expected CLI parameters:");
-                Console.Error.WriteLine("* Storage root folder");
-                Console.Error.WriteLine("* Kusto Database Name");
-                Console.Error.WriteLine("* Kusto Table Name");
-                Console.Error.WriteLine("* Partition Key Column Name");
-                Console.Error.WriteLine("* Kusto Ingestion URI");
-                Console.Error.WriteLine();
-                Console.Error.WriteLine("At least one Kusto Ingestion URI is expected");
-                Console.Error.WriteLine("If one is provided, partitioning hint are passed "
-                    + "during ingestion");
-                Console.Error.WriteLine("If two are provided, the second one doesn't get"
-                    + "partitioning hint");
+                var firstParam = args[0];
+
+                if (firstParam.Length == 2 && firstParam[0] == '-')
+                {
+                    var flow = firstParam[1];
+
+                    switch (flow)
+                    {
+                        case 'p':
+                            await RunPartitioningFlowAsync(args);
+                            return;
+                        case 's':
+                            return;
+                        default:
+                            throw new NotSupportedException($"Flow '{flow}' isn't supported");
+                    }
+                }
             }
-            else
+            DisplayHelp();
+        }
+
+        private static async Task RunPartitioningFlowAsync(string[] args)
+        {
+            if (args.Length >= 5)
             {
-                var storageUrl = args[0];
+                var storageUrl = args[1];
                 var nonSasStorageUrl = storageUrl.Split('?').First();
-                var databaseName = args[1];
-                var tableName = args[2];
-                var partitionKeyColumn = args[3];
-                var ingestionUri1 = args[4];
-                var ingestionUri2 = args.Length >= 6 ? args[5] : string.Empty;
+                var databaseName = args[2];
+                var tableName = args[3];
+                var partitionKeyColumn = args[4];
+                var ingestionUri1 = args[5];
+                var ingestionUri2 = args.Length >= 7 ? args[6] : string.Empty;
                 var orchestrator = new BulkOrchestrator(
                     storageUrl,
                     databaseName,
@@ -45,6 +55,35 @@
 
                 await orchestrator.RunAsync();
             }
+            else
+            {
+                DisplayHelp();
+            }
+        }
+
+        private static void DisplayHelp()
+        {
+            Console.Error.WriteLine("Expected CLI parameters for partitioning:");
+            Console.Error.WriteLine("* '-p'");
+            Console.Error.WriteLine("* Storage root folder");
+            Console.Error.WriteLine("* Kusto Database Name");
+            Console.Error.WriteLine("* Kusto Table Name");
+            Console.Error.WriteLine("* Partition Key Column Name");
+            Console.Error.WriteLine("* Kusto Ingestion URI");
+            Console.Error.WriteLine();
+            Console.Error.WriteLine("At least one Kusto Ingestion URI is expected");
+            Console.Error.WriteLine("Partitioning hint is passed on the first one only");
+
+            Console.Error.WriteLine();
+            Console.Error.WriteLine("Expected CLI parameters for pre-sharding:");
+            Console.Error.WriteLine("* '-s'");
+            Console.Error.WriteLine("* Storage root folder");
+            Console.Error.WriteLine("* Kusto Database Name");
+            Console.Error.WriteLine("* Kusto Table Name");
+            Console.Error.WriteLine("* Kusto Ingestion URI");
+            Console.Error.WriteLine();
+            Console.Error.WriteLine("At least one Kusto Ingestion URI is expected");
+            Console.Error.WriteLine("Pre-sharding is done on the first one only");
         }
     }
 }
