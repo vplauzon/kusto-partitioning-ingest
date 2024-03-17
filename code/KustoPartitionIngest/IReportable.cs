5d0a65746684028955845e5c23ce12db4d4bf25d
vplauzon <vincent_lauzon@yahoo.com.au>
2024-03-17 15:27:19 -0400

    .

---
+++ code/KustoPartitionIngest/IReportable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoPartitionIngest
+{
+    internal interface IReportable
+    {
+        IImmutableDictionary<string, string> GetReport();
+    }
+}
