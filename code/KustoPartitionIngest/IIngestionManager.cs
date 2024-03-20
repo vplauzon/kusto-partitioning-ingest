1cfd961c9143f41adbb87692c413f65b862c267d
vplauzon <vincent_lauzon@yahoo.com.au>
2024-03-20 10:46:56 -0400

    .

---
+++ code/KustoPartitionIngest/IIngestionManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoPartitionIngest
+{
+    internal interface IIngestionManager : IAsyncDisposable
+    {
+        Task QueueIngestionAsync(
+            IEnumerable<Uri> blobUris,
+            DateTime? creationTime);
+    }
+}
