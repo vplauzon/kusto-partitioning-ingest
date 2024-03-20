5a2afdbc7947414b768d4cd008f5e0e3ee2fd74c
vplauzon <vincent_lauzon@yahoo.com.au>
2024-03-20 18:13:53 -0400

    .

--- code/KustoPartitionIngest/DmBackedIngestionManager.cs
+++ code/KustoPartitionIngest/DmBackedIngestionManager.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Azure.Core;
+using Azure.Storage.Blobs.Models;
+using Kusto.Data;
+using Kusto.Ingest;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +12,24 @@ namespace KustoPartitionIngest
 {
     internal class DmBackedIngestionManager : IIngestionManager
     {
+        private readonly IKustoQueuedIngestClient _ingestClient;
+        private readonly string _databaseName;
+        private readonly string _tableName;
+
+        public DmBackedIngestionManager(
+            TokenCredential credentials,
+            Uri ingestionUri,
+            string databaseName,
+            string tableName)
+        {
+            var builder = new KustoConnectionStringBuilder(ingestionUri.ToString())
+                .WithAadAzureTokenCredentialsAuthentication(credentials);
+
+            _ingestClient = KustoIngestFactory.CreateQueuedIngestClient(builder);
+            _databaseName = databaseName;
+            _tableName = tableName;
+        }
+
         #region IAsyncDisposable
         async ValueTask IAsyncDisposable.DisposeAsync()
         {
