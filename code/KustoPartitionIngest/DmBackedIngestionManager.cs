96b4fee1a56996d6755259c24a619c96767729cd
vplauzon <vincent_lauzon@yahoo.com.au>
2024-03-20 18:11:54 -0400

    .

---
+++ code/KustoPartitionIngest/DmBackedIngestionManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoPartitionIngest
+{
+    internal class DmBackedIngestionManager : IIngestionManager
+    {
+        #region IAsyncDisposable
+        async ValueTask IAsyncDisposable.DisposeAsync()
+        {
+            await ValueTask.CompletedTask;
+        }
+        #endregion
+
+        #region IIngestionManager
+        Task IIngestionManager.QueueIngestionAsync(
+            IEnumerable<Uri> blobUris,
+            DateTime? creationTime)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
