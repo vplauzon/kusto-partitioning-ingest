79a9302b3e93e37917eb3b2dff12b0bdbf44c348
vplauzon <vincent_lauzon@yahoo.com.au>
2024-03-22 19:11:21 -0400

    .

---
+++ code/KustoPartitionIngest/InProcManagedIngestion/IngestionCommandManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoPartitionIngest.InProcManagedIngestion
+{
+    internal class IngestionCommandManager
+    {
+        public Task ExecuteIngestionAsync(string commandText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ code/KustoPartitionIngest/InProcManagedIngestion/OperationManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoPartitionIngest.InProcManagedIngestion
+{
+    internal class OperationManager
+    {
+        public Task OperationCompletedAsync(string operationId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
